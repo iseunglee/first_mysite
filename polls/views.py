ba705ba1c6fcefcc45d9d7b8a40a0df5b9895612
iseunglee <iseunglee99@gmail.com>
2024-04-01 20:52:45 +0900

    4/1 수업내용

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Question, Choice
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
+from django.urls import reverse
+from django.db.models import F
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")
@@ -14,15 +16,50 @@ def index(request):
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+    # choice_count = len(question.choice_set.all())
+    # content_1 = question.choice_set.all()[0]
+    question_list = Question.objects.all()
+    context = {
+         "question" : question,
+         "question_list" : question_list,
+    
 
+    }
+    return render(request, "polls/detail.html", context)
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    #response = "You're looking at the results of question %s."
+    #return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    # choice 데이터에서 해당하는 값에 votes를 +1
+    #c = Choice.objects.get(pk=8)
+    #c.votes += 1
+    #c.save()
+    #return HttpResponse("You're voting on question %s." % question_id)
 
 # question_id 말고 다른 이름으로 받아도 될까?
 # 숫자가 아닌게 들어오면 어떻게 되지?
